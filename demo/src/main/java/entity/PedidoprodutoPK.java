82460b31a5d0550b738209ca4e337a3c6fe2460a
Paulo Amorim <paulo.amorim@ipvc.pt>
2024-04-15 16:01:21 +0100

    qwert

--- demo/src/main/java/entity/PedidoprodutoPK.java
+++ demo/src/main/java/entity/PedidoprodutoPK.java
@@ -1,6 +1,13 @@
 package entity;
 
+import jakarta.persistence.Column;
+
 import java.io.Serializable;
 
 public class PedidoprodutoPK implements Serializable {
+    @Column(name = "idpedido")
+    private Long idPedido;
+
+    @Column(name = "idproduto")
+    private Long idProduto;
 }
